6248f5c1df523f4a37812fb2d61380cab04bdf9f
Keshav vats <keshavvats42@gmail.com>
2025-01-12 14:02:21 +0530

    var dump function is used

--- ist.php
+++ ist.php
@@ -1,3 +1,14 @@
 <?php
 echo "Hello World";
+echo '<br>how are you';
+$a=12;
+$b=3;
+$c=$a+$b;
+echo "<br> Sum is $c";
+$ambala=12.2;
+echo"<br>";
+var_dump($ambala);
+$chd="Keshav";
+echo"<br>";
+var_dump($chd);
 ?>
